403ecf304e2c2e6428f29a485b003f96ba8b373d
magickurasho <kurasho.tanya@gmail.com>
2024-06-08 18:55:01 +0300

    Add new feature TestJS/types/index.d.ts

---
+++ TestJS/types/index.d.ts
@@ -0,0 +1,38 @@
+// Type definitions for TestJS
+// Definitions by: <https://github.com/taoqf>
+
+// Core
+export { default as TestJS } from '@TestJS/core'
+
+// Stores
+export { default as ReduxStore } from '@TestJS/store-redux'
+
+// UI plugins
+export { default as Dashboard } from '@TestJS/dashboard'
+export { default as DragDrop } from '@TestJS/drag-drop'
+export { default as FileInput } from '@TestJS/file-input'
+export { default as Informer } from '@TestJS/informer'
+export { default as ProgressBar } from '@TestJS/progress-bar'
+export { default as StatusBar } from '@TestJS/status-bar'
+
+// Acquirers
+export { default as Dropbox } from '@TestJS/dropbox'
+export { default as Box } from '@TestJS/box'
+export { default as GoogleDrive } from '@TestJS/google-drive'
+export { default as Instagram } from '@TestJS/instagram'
+export { default as Url } from '@TestJS/url'
+export { default as Webcam } from '@TestJS/webcam'
+export { default as ScreenCapture } from '@TestJS/screen-capture'
+
+// Uploaders
+export { default as AwsS3 } from '@TestJS/aws-s3'
+export { default as AwsS3Multipart } from '@TestJS/aws-s3-multipart'
+export { default as Transloadit } from '@TestJS/transloadit'
+export { default as Tus } from '@TestJS/tus'
+export { default as XHRUpload } from '@TestJS/xhr-upload'
+
+// Miscellaneous
+export { default as Form } from '@TestJS/form'
+export { default as GoldenRetriever } from '@TestJS/golden-retriever'
+export { default as ReduxDevTools } from '@TestJS/redux-dev-tools'
+export { default as ThumbnailGenerator } from '@TestJS/thumbnail-generator'
